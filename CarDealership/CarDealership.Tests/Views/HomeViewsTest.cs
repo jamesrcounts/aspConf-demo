77450c30a5b206b6e578c557adbe0f33042a9b25
Jim Counts <fritzjob18@nethere.com>
2012-07-12 07:23:44 -0700

    First View Test

---
+++ CarDealership/CarDealership.Tests/Views/HomeViewsTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using ApprovalTests.Asp;
+using ApprovalTests.Asp.Mvc;
+using ApprovalTests.Reporters;
+using CarDealership.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[assembly: UseReporter(typeof(FileLauncherReporter), typeof(TortoiseDiffReporter))]
+
+namespace CarDealership.Tests.Views
+{
+    [TestClass]
+    public class HomeViewsTest
+    {
+        [TestMethod]
+        public void TestIndexView()
+        {
+            // Supply route to MVC action
+            PortFactory.MvcPort = 2080;
+            Func<ActionResult> actionDelegate = new HomeController().Index;  // This delegate is never invoked.  Its just a way to pass around metadata about the route
+
+            // Test action Result (RED)
+            MvcApprovals.VerifyMvcPage(actionDelegate);
+
+            // Review failing result
+            // Approve Result if appropriate
+            // Test Again (GREEN)
+            // Refactor
+        }
+
+        [TestMethod]
+        public void TestAboutView()
+        {
+        }
+    }
+}
