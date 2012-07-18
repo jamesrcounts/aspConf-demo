df39c3d5df0ba7bb5a879a538c763d187b266052
Jim Counts <fritzjob18@nethere.com>
2012-07-18 07:08:30 -0700

    First View Test

---
+++ CarDealership/CarDealership.Tests/Views/HomeViewsTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ApprovalTests.Asp;
+using ApprovalTests.Asp.Mvc;
+using ApprovalTests.Reporters;
+using System.Web.Mvc;
+using CarDealership.Controllers;
+
+[assembly: UseReporter(typeof(FileLauncherReporter), typeof(TortoiseDiffReporter))]
+namespace CarDealership.Tests.Views
+{
+    [TestClass]
+    public class HomeViewsTest
+    {
+        [TestMethod]
+        public void IndexViewTest()
+        {
+            PortFactory.MvcPort = 43453;
+            MvcApprovals.VerifyMvcPage(new HomeController().Index);
+        }
+
+        [TestMethod]
+        public void AboutViewTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void CarsViewTest()
+        {
+            // will have some data
+        }
+    }
+}
