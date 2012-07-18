a4890071fc24a2db81c7dadf8b7aad0933766c25
Jim Counts <fritzjob18@nethere.com>
2012-07-18 11:04:39 -0700

    Implemented Second View Test.

--- CarDealership/CarDealership.Tests/Views/HomeViewsTest.cs
+++ CarDealership/CarDealership.Tests/Views/HomeViewsTest.cs
@@ -14,31 +14,18 @@ using CarDealership.Controllers;
 namespace CarDealership.Tests.Views
 {
     [TestClass]
-    public class HomeViewsTest
+    public class HomeViewsTest : MvcViewTest
     {
         [TestMethod]
         public void IndexViewTest()
         {
-            // make our views available? must be on a websever CTRL-f5
-            // give route to action which produces the view
-            PortFactory.MvcPort = 42110;
-            Func<ActionResult> actionDelegate = new HomeController().Index;
-            // home controller never used
-            // delegate is never invoked.
-            // just metadata
-
-            // run the test (RED)
-            MvcApprovals.VerifyMvcPage(actionDelegate);
-
-            // approve results
-            // run again (GREEN)
-            // refactor
+            MvcApprovals.VerifyMvcPage(new HomeController().Index);
         }
 
         [TestMethod]
         public void AboutViewTest()
         {
-
+            MvcApprovals.VerifyMvcPage(new HomeController().About);
         }
 
         [TestMethod]
---
+++ CarDealership/CarDealership.Tests/Views/MvcViewTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ApprovalTests.Asp;
+
+namespace CarDealership.Tests.Views
+{
+    public class MvcViewTest
+    {
+        public MvcViewTest()
+        {
+            PortFactory.MvcPort = 42110;
+        }
+    }
+}
