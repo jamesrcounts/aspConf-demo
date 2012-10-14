8a6033c18af9807d61188d8bf838762a20768723
James Counts <fritzjob18@nethere.com>
2012-10-14 12:36:36 -0700

    First ViewTest

---
+++ CarDealership/CarDealership.Tests/Views/HomeViewsTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Web.Mvc;
+using ApprovalTests.Asp.Mvc;
+using ApprovalTests.Reporters;
+using CarDealership.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CarDealership.Tests.Views
+{
+    [TestClass]
+    [UseReporter(typeof(TortoiseDiffReporter), typeof(FileLauncherReporter))]
+    public class HomeViewsTest
+    {
+        [TestMethod]
+        public void IndexViewTest()
+        {
+            // make the view available
+
+            // provide route
+            ApprovalTests.Asp.PortFactory.MvcPort = 50978;
+            var controller = new HomeController();
+            Func<ActionResult> method = controller.Index;
+
+            // test it (red)
+            MvcApprovals.VerifyMvcPage(method);
+
+            // review it approve if appropriate
+            // test it (green)
+            // refactor.
+        }
+
+        [TestMethod]
+        public void AboutViewTest()
+        {
+            // aboutViewTest
+        }
+
+        [TestMethod]
+        public void CarsViewTest()
+        {
+            // some test with data, should be ineresting
+        }
+    }
+}
--- CarDealership/CarDealership/Controllers/HomeController.cs
+++ CarDealership/CarDealership/Controllers/HomeController.cs
@@ -1,22 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace CarDealership.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult About()
+        {
+            return View();
+        }
+
         public ActionResult Index()
         {
             ViewBag.Message = "Welcome to ASP.NET MVC!";
 
-            return View();
-        }
-
-        public ActionResult About()
-        {
             return View();
         }
     }
